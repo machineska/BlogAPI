a673bb25f8279d3c2990baae55675d0cbb9c328a
machineska <alternative.xen@gmail.com>
2022-09-16 17:42:21 +0800

    load blog

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
 @app.get("/")
-def index():
-    return {'data': 'blog list'}
+def index(limit: int = 50, published: bool = True, sort: Optional[str] = None):
+    """Home Page Website"""
+    if published:
+        return {'data': f'{limit} published data blog list'}
+    else:
+        return {'data': f'{limit} data blog list'}
 
 
 @app.get('/blog/unpublished')
@@ -22,11 +28,22 @@ def show(id: int):
 
 
 @app.get('/blog/{id}/comments')
-def comments(id):
+def comments(id, limit=10):
     """Fetch comments of blog with id = id"""
     return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published_at: Optional[bool]
 
 
 @app.get("/about")
 def about():
     return {'data': 'about'}
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f'Blog is created with title {blog.title}'}
